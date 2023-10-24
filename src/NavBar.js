e49e0ab4ea74b22910486e175385ad1511fab714
kashkom98 <komandurisrinivasa.p@northeastern.edu>
2023-10-24 00:06:07 -0400

    navbar added

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Articles</li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/articles">Articles</Link></li>
             </ul>
         </nav>
     )
